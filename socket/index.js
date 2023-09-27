b642e32f8a78a40e4aec4de987755c614f91bb77
anya <riidloa@gmail.com>
2023-09-27 20:30:54 +0700

    handle callback on sending message as ack

--- socket/index.js
+++ socket/index.js
@@ -3,18 +3,23 @@ module.exports = (http) => {
     cors: "*"
   })
   io.on("connection", (socket) => {
-    console.log("user connected")
+    console.log("[ON: connection] user connected")
     socket.on("disconnect", () => {
-      console.log("user disconnect")
+      console.log("[ON: disconnect] user disconnect")
     })
 
     socket.on("userId", (id) => {
-      console.log(`user ${id} joined`)
+      console.log(`[ON: userId] user ${id} joined`)
     })
 
-    socket.on("send-message", (obj) => {
-      console.log(obj)
-      io.emit('receive-message', obj)
+    socket.on("send-message", (data, callback) => {
+      console.log("[ON: send-message] data:", data)
+      
+      // simulate loading on sending message before reply ack
+      setTimeout(()=> {
+        callback({status: true})
+        io.emit('receive-message', data)
+      }, 2000)
     })
   })
 }
