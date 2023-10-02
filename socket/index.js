56c6028cf87170fce4d8d61b8cdc49fd96f5b7f9
anya <riidloa@gmail.com>
2023-10-03 03:08:40 +0700

    handle basic private message

--- socket/index.js
+++ socket/index.js
@@ -2,24 +2,40 @@ module.exports = (http) => {
   const io = require("socket.io")(http, {
     cors: "*"
   })
+
+  const connectedUsers = {}
+
   io.on("connection", (socket) => {
     console.log("[ON: connection] user connected")
+
     socket.on("disconnect", () => {
       console.log("[ON: disconnect] user disconnect")
     })
 
-    socket.on("userId", (id) => {
-      console.log(`[ON: userId] user ${id} joined`)
+    socket.on("register-id", (userId) => {
+      console.log(`[ON: userId] user ${userId} joined`)
+      if (!connectedUsers.hasOwnProperty(userId))
+        connectedUsers[userId] = socket.id
+
+      console.log("update connectedUsers: ", connectedUsers)
     })
 
     socket.on("send-message", (data, callback) => {
       console.log("[ON: send-message] data:", data)
-      
-      // simulate loading on sending message before reply ack
-      setTimeout(()=> {
-        callback({status: true})
-        io.emit('receive-message', data)
-      }, 2000)
+
+      if (connectedUsers.hasOwnProperty(data.recepient)) {
+        const targetSocketId = connectedUsers[data.recepient]
+
+        socket.to(targetSocketId).emit("receive-message", {
+        // socket.emit("receive-message", {
+          content: data.content,
+          sender: data.sender,
+          time: data.time
+        })
+        callback({ status: true })
+      } else {
+        callback({ status: false })
+      }
     })
   })
 }
